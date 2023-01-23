6c4c502d00c1714519c3cef46dc8c2896557e6ea
matulibe <57047770+matulibe@users.noreply.github.com>
2023-01-23 01:08:12 -0300

    asfdfa

--- demo/src/main/java/com/webproject/demo/Service/UsuarioService.java
+++ demo/src/main/java/com/webproject/demo/Service/UsuarioService.java
@@ -1,8 +1,13 @@
 package com.webproject.demo.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.webproject.demo.Repository.UsuariosRepository;
 
 @Service
 public class UsuarioService {
     
+    @Autowired
+    private UsuariosRepository usuariosRepository;                                                                                                                                                                                            
 }
